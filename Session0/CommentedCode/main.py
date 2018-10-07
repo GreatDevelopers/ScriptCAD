5d4aff8238a56b239ce62073d28ca00706206a2b
marvellouschandan <marvellouschandan@gmail.com>
2018-10-07 21:22:49 +0530

    Shorten lines which are more than 80

--- Session0/CommentedCode/main.py
+++ Session0/CommentedCode/main.py
@@ -10,15 +10,20 @@ from GameParameters import *     # imports the parameters to be used here
 thisGen = []
 nextGen = []
 
-# initGrid() is a function included in library GameOfLife.py which gives the list (which is passed), an initial state which is completely random.
+# initGrid() is a function included in library GameOfLife.py which gives 
+# the list (which is passed), an initial state which is completely random.
 
 initGrid(ROWS, COLS,  thisGen)
 
-# printGen() is a function included in library GameOfLife.py which prints the generation (2D matrix).
+# printGen() is a function included in library GameOfLife.py which prints 
+# the generation (2D matrix).
 
-# processNextGen() generates the next generation (next 2D matrix) which is totaly dependent upon the previous generation (current generation 2D matrix), and hence both (thisGen[] and nextGen[]) are arguments to this function.
+# processNextGen() generates the next generation (next 2D matrix) which is 
+# totaly dependent upon the previous generation (current generation 2D matrix),
+# and hence both (thisGen[] and nextGen[]) are arguments to this function.
 
-# sleep() is a function in the library "time" which takes arguments in seconds. It produces delay between the previous generation and the next generation.
+# sleep() is a function in the library "time" which takes arguments in seconds.
+# It produces delay between the previous generation and the next generation.
 
 nextGen = copy.deepcopy(thisGen)
 
@@ -29,8 +34,9 @@ for gens in range(GENERATIONS):
     printGen(ROWS, COLS, nextGen, gens)
     input()
 
-    # Once current generation is done printing. The next generation is now proceessed the next generation to be printed out next.
-    # At last thisGen has been assigned the value of nextGen for getting copied.
+# Once current generation is done printing. The next generation is now 
+# proceessed the next generation to be printed out next.
+# At last thisGen has been assigned the value of nextGen for getting copied.
 
     thisGen = copy.deepcopy(nextGen)
 
